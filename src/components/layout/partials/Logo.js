183ef7380f851725fd8affc4dd6e0f1cb41a7027
Benjamin Arntzen <zorlin@gmail.com>
2021-02-15 22:40:21 +0800

    Fix logo size, remove video, remove 2 testimonials

--- src/components/layout/partials/Logo.js
+++ src/components/layout/partials/Logo.js
@@ -23,8 +23,8 @@ const Logo = ({
           <Image
             src={require('./../../../assets/images/logo.png')}
             alt="Open"
-            width={32}
-            height={32} />
+            width={72}
+            height={72} />
         </Link>
       </h1>
     </div>
