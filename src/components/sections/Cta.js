f8516fd03cb95e9e96991dfc0e39332a07918505
Benjamin Arntzen <zorlin@gmail.com>
2021-02-15 13:56:35 +0800

    Update footer

--- src/components/sections/Cta.js
+++ src/components/sections/Cta.js
@@ -55,13 +55,12 @@ const Cta = ({
             <h3 className="m-0">
               Ready to join?
               </h3>
-          </div>
-          <div className="cta-action">
-            <Input id="newsletter" type="email" label="Subscribe" labelHidden hasIcon="right" placeholder="Click me">
-              <svg width="16" height="12" xmlns="http://www.w3.org/2000/svg">
+	    <h3 className="m-0">
+              <a href="mailto:benjamin@riff.cc"><svg width="16" height="12" xmlns="http://www.w3.org/2000/svg">
                 <path d="M9 5H1c-.6 0-1 .4-1 1s.4 1 1 1h8v5l7-6-7-6v5z" fill="#376DF9" />
               </svg>
-            </Input>
+              Click me.</a>
+              </h3>
           </div>
         </div>
       </div>
